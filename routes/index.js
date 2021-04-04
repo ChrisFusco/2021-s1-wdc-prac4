48d682c404cffa918bd6a38fe4b83b0563f8bda6
CJcool06 <cjcool06@yahoo.com.au>
2021-04-04 17:06:04 +0930

    last

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var data = [];
+router.get('/last.txt', function(req, res, next) {
+  if (data.length == 0) {
+    res.send('<!DOCTYPE html> \
+    <html> \
+        <head> \
+            <title>Last</title> \
+        </head> \
+        <body> \
+        </body> \
+    </html> \
+    ');
+  }
+  else {
+    res.send('<!DOCTYPE html> \
+    <html> \
+        <head> \
+            <title>Last</title> \
+        </head> \
+        <body> \
+        ' + data[0] + ' \
+        </body> \
+    </html> \
+    ');
+  }
+  data[0] = new Date().toString();
+});
+
 module.exports = router;
